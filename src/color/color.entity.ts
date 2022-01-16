265c0e2d0ba8a4e47a493dc542664fb6707533c7
Eaeajj <eccentricmadm@gmail.com>
2022-01-16 23:15:52 +0300

    Add hex column & constraint

--- src/color/color.entity.ts
+++ src/color/color.entity.ts
@@ -1,17 +1,34 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  Unique,
+  Check,
+} from "typeorm";
 import { ApiProperty } from "@nestjs/swagger";
 import { Media } from "src/media/media.entity";
 import { Stock } from "src/stocks/stock.entity";
 
 @Entity({ name: "colors" })
+@Check("check_hex", "hex ~ '^#[a-f0-9]{2}[a-f0-9]{2}[a-f0-9]{2}$'")
+@Unique("colors_name_hex_key", ["name", "hex"])
 export class Color {
   @ApiProperty({ example: "1", description: "unique id" })
   @PrimaryGeneratedColumn()
   id: number;
 
   @ApiProperty({ example: "YELLOW", description: "color name" })
-  @Column({ type: "varchar", length: 30, unique: true })
+  @Column({
+    type: "varchar",
+    length: 30,
+    unique: true,
+  })
   name: string;
+
+  @ApiProperty({ example: "#ffffff", description: "color hex code" })
+  @Column({ type: "varchar", length: 7, unique: true })
+  hex: string;
 
   @OneToMany(() => Media, (media) => media.colorId)
   media: Media[];
