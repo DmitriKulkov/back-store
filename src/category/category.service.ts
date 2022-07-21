5b0e60db5c4635e7b99b4e81550d1a16c659c00a
SimplePC <simplepc@pop-os.localdomain>
2022-07-21 14:22:10 +0300

    Add Category Service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Category} from "./category.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(
+        @InjectRepository(Category) private categoryRepo: Repository<Category>
+    ) {}
+
+    async getAll(){
+        const cat = await this.categoryRepo.find()
+        return cat
+    }
+}
